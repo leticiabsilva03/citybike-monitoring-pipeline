d044f32dea33e97fd4952b6f2b9b5c49c2ec8208
Leticia Batista Silva <leticiabsilva03@gmail.com>
2025-05-05 21:22:56 -0300

    feat(bronze): collect CityBikes data using Airflow and MinIO

--- dags/collector_station_status.py
+++ dags/collector_station_status.py
@@ -2,23 +2,33 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 from minio import Minio
-import requests, json
+from io import BytesIO
+import requests
 
 def fetch_stations():
     client = Minio(
-        "localhost:9000",
-        access_key="minio",
-        secret_key="minio123",
+        "minio:9000",
+        access_key="minioadmin",
+        secret_key="minioadmin123",
         secure=False
     )
     url = "https://api.citybik.es/v2/networks/citi-bike-nyc"
-    data = requests.get(url).json()
+    resp = requests.get(url)
+    resp.raise_for_status()
+    data = resp.content
     ts = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
-    payload = json.dumps(data).encode('utf-8')
-    client.put_object("bronze", f"stations/{ts}.json", payload, len(payload))
+
+    payload = BytesIO(data)
+    client.put_object(
+        bucket_name="bronze",
+        object_name=f"stations/{ts}.json",
+        data=payload,
+        length=len(data),
+        content_type="application/json"
+    )
 
 with DAG(
-    "station_status_bronze",
+    "bronze",
     start_date=datetime(2025,5,4),
     schedule_interval="*/5 * * * *",
     catchup=False
